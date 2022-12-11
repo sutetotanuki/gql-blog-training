441f458a85198b31f3402db196b74fac6af4eb97
cm-nishida-masayuki <nishida.masayuki@classmethod.jp>
2022-12-11 10:00:32 +0900

    test

--- backend/README.md
+++ backend/README.md
@@ -70,5 +70,5 @@ Nest is an MIT-licensed open source project. It can grow thanks to the sponsors 
 
 ## License
 
-aaaaaaa
+aaaaaaaa
 Nest is [MIT licensed](LICENSE).
--- infra/modules/cloud-build/cloud-build.tf
+++ infra/modules/cloud-build/cloud-build.tf
@@ -17,8 +17,8 @@ resource "google_cloudbuild_trigger" "deploy-backend-training-app" {
     }
   }
 
-  included_files = ["blog-deploy-cloud-run/backend/**"]
-  filename       = "blog-deploy-cloud-run/backend/cloudbuild.yml"
+  included_files = ["backend/**"]
+  filename       = "backend/cloudbuild.yml"
   substitutions = {
     "_REGION"                         = var.region
     "_CLOUDSQL_INSTANCE_FULL_NAME"    = var.cloudsql_instance_fullname
